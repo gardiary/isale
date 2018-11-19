09b63d14c84f85bf33bea51b6a365c1612946e3d
mwiyono <mujoko.wiyono@openet.com>
2018-11-20 02:20:02 +0800

    Add CORS

--- src/main/java/com/ojolali/isale/IsaleApplication.java
+++ src/main/java/com/ojolali/isale/IsaleApplication.java
@@ -1,12 +1,21 @@
 package com.ojolali.isale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class IsaleApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(IsaleApplication.class, args);
 	}
+	    
 }
